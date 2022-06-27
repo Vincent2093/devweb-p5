64dfa16c47364be4c71ca2efea618498aa6d52c8
Vince2093 <veluna@outlook.fr>
2022-06-27 12:46:04 +0200

    add product on localstorage when addToCart button clicked

--- front/js/index.js
+++ front/js/index.js
@@ -11,24 +11,24 @@ function addAllProducts(products) {
 
 function displayProduct(product) {
     const productAnchor = document.createElement("a")
-    document.getElementById("items").appendChild(productAnchor)
     productAnchor.href = `./product.html?id=${product._id}`
+    document.getElementById("items").appendChild(productAnchor)
   
     const productArticle = document.createElement("article")
     productAnchor.appendChild(productArticle)
   
     const productImage = document.createElement("img")
-    productArticle.appendChild(productImage)
     productImage.src = product.imageUrl
     productImage.alt = product.altTxt
+    productArticle.appendChild(productImage)
   
     const productName = document.createElement("h3")
-    productArticle.appendChild(productName)
     productName.classList.add("productName")
     productName.textContent = product.name
+    productArticle.appendChild(productName)
   
     const productDescription = document.createElement("p")
-    productArticle.appendChild(productDescription)
     productDescription.classList.add("productDescription")
     productDescription.textContent = product.description    
+    productArticle.appendChild(productDescription)
 }
--- front/js/product.js
+++ front/js/product.js
@@ -1,32 +1,66 @@
 const url = new URL(document.location)
 const id = url.searchParams.get("id")
 
+const productImage = document.createElement("img")
+const productName = document.getElementById("title")
+const productPrice = document.getElementById("price")
+const productDescription = document.getElementById("description")
+const productColors = document.getElementById("colors")
+const btnCart = document.getElementById("addToCart")
+
 fetch(`http://localhost:3000/api/products/${id}`)
     .then(response => response.json())
     .then(product => displayProductDetails(product))
     .catch(error => alert(error = "Ce produit n'a pu être affiché, merci de bien vouloir contacter notre assistance."))
 
 function displayProductDetails(product) {
-    const productImage = document.createElement("img")
-    document.querySelector(".item__img").appendChild(productImage)
+    
     productImage.src = product.imageUrl
     productImage.alt = product.altTxt
+    document.querySelector(".item__img").appendChild(productImage)
 
-    const productName = document.getElementById("title")
     productName.textContent = product.name
 
-    const productPrice = document.getElementById("price")
     productPrice.textContent = product.price
-
-    const productDescription = document.getElementById("description")
+    
     productDescription.textContent = product.description
 
-    const productColors = document.getElementById("colors")
-
     product.colors.forEach((color) => {
-    const productColorsOption = document.createElement("option")
-    productColors.appendChild(productColorsOption)
-    productColorsOption.value = color
-    productColorsOption.textContent = color
+        const productColorsOption = document.createElement("option")
+        productColorsOption.value = color
+        productColorsOption.textContent = color
+        productColors.appendChild(productColorsOption)
     })
+}
+
+btnCart.addEventListener("click", sendToCart)
+
+function sendToCart(){
+        const color = document.getElementById("colors").value
+        const quantity = document.getElementById("quantity").value
+
+        if (color === "" && quantity == 0) {
+            alert("Merci de choisir une couleur et une quantité supérieur à 0 !")
+            return
+        } else if (color === ""){
+            alert("Merci de choisir une couleur !")
+            return
+        } else if (quantity == 0 || quantity > 100){
+            alert("Merci de choisir une quantité comprise entre 1 et 100 !")
+            return
+        }
+        
+        const orderData = {
+            id: id,
+            imgAlt : productImage.alt,
+            imgSrc : productImage.src,
+            productName: productName.innerHTML,
+            color: color,
+            price: productPrice.innerHTML,
+            quantity: quantity
+        }
+
+        localStorage.setItem(id, JSON.stringify(orderData))
+
+        window.location.href = "cart.html";
 }
